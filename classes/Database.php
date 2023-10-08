8fef1faa0ca5f0c119740ec559e852b20a5e5a84
szabomarcel <szabomarcell34@gmail.com>
2023-10-08 21:20:14 +0200

    first commit

---
+++ classes/Database.php
@@ -0,0 +1,48 @@
+<?php
+
+class Database {
+
+    private $db = null;
+
+    public function __construct($host, $username, $pass, $db) {
+        $this->db = new mysqli($host, $username, $pass, $db);
+    }
+
+    public function login($name, $pass) {
+        //-- jelezzük a végrehajtandó SQL parancsot
+        $stmt = $this->db->prepare('SELECT * FROM users WHERE users.name LIKE ?;');
+        //-- elküldjük a végrehajtáshoz szükséges adatokat
+        $stmt->bind_param("s", $name);
+
+        if ($stmt->execute()) {
+            //-- sikeres végrehajtás után lekérjük az adatokat
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            if (password_verify($pass, $row['password'])) {
+                //-- felhasználónév és jelszó helyes
+                $_SESSION['username'] = $row['name'];
+                $_SESSION['login'] = true;
+            } else {
+                $_SESSION['username'] = '';
+                $_SESSION['login'] = false;
+            }
+            // Free result set
+            $result->free_result();
+        }
+        return false;
+    }
+
+    public function register($name, $pass) {
+        $password = password_hash($pass, PASSWORD_BCRYPT);
+        var_dump($password);
+        $stmt = $this->db->prepare("INSERT INTO `users` (`name`, `password`) VALUES (?, ?);");
+        $stmt->bind_param("sb", $name, password_hash($pass, PASSWORD_BCRYPT));
+        if ($stmt->execute()) {
+            $_SESSION['login'] = true;
+            //header("Location: index.php");
+        } else {
+            $_SESSION['login'] = false;
+            echo '<p>Rögzítés sikertelen!</p>';
+        }
+    }
+}
---
+++ tartalom.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_SESSION['login']) {
+    echo $_SESSION['username'] . ' belépett ';
+} else {
+    if (filter_input(INPUT_GET, "menu") == "regisztracio") {
+        require_once './pages/regisztracio.php';
+    } else {
+        require_once './pages/login.php';
+    }
+}
